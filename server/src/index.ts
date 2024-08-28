02c39d843984f169ad7ea14500646ed7374da306
Artem <artemisia8192@gmail.com>
2024-08-28 13:32:22 +0200

    Another try to configure CORS

--- server/src/index.ts
+++ server/src/index.ts
@@ -48,7 +48,7 @@ useServer({ schema, context }, wsServer);
 const startServer = async () => {
 	await apolloServer.start()
 	
-	app.use('/api/graphql', cors<cors.CorsRequest>(), express.json(), expressMiddleware(apolloServer, { context }))
+	app.use('/api/graphql', cors({ origin: '*', credentials: true }), express.json(), expressMiddleware(apolloServer, { context }))
 
 	await new Promise((resolve, reject) => httpServer.listen({ port: 3002 }, () => resolve(undefined)))
 	console.log("Server listens on port 3002")
