e09d5cc4207db0e8dd9558b4825cf47e9d5fe639
Artem <artemisia8192@gmail.com>
2024-08-30 21:10:51 +0200

    Changed apollo client

--- src/graphql/gqlClient.ts
+++ src/graphql/gqlClient.ts
@@ -1,44 +1,18 @@
-import { ApolloClient, InMemoryCache, HttpLink, split } from '@apollo/client';
-import { GraphQLWsLink } from '@apollo/client/link/subscriptions';
-import { createClient } from 'graphql-ws';
-import { getMainDefinition } from '@apollo/client/utilities';
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+import { ApolloClient, InMemoryCache } from '@apollo/client'
 
 
-const production = process.env.NEXT_PUBLIC_ENV_PRODUCTION
-
-console.log("PRODUCTION MODE = ", production)
-console.log("HTTP_SERVER_PRODUCTION_URI = ", process.env.NEXT_PUBLIC_HTTP_SERVER_PRODUCTION_URI)
-
-// Create an HTTP link for regular queries and mutations
-const httpLink = new HttpLink({
-  uri: production ? process.env.NEXT_PUBLIC_HTTP_SERVER_PRODUCTION_URI! : "http://localhost:4000",
-});
-
-// Create a WebSocket link for subscriptions
-const wsLink = new GraphQLWsLink(
+const link = new GraphQLWsLink(
   createClient({
-    url: production ? process.env.NEXT_PUBLIC_WS_SERVER_PRODUCTION_URI! : "ws://localhost:4000",
-  })
+		url: process.env.NEXT_PUBLIC_ENV_PRODUCTION ? process.env.NEXT_PUBLIC_GRAPHQL_API_PRODUCTION_URI! : "ws://localhost:4000/graphql",
+  }),
 );
 
-// Use split to send data to each link based on the operation type
-const splitLink = split(
-  ({ query }) => {
-    const definition = getMainDefinition(query);
-    return (
-      definition.kind === 'OperationDefinition' &&
-      definition.operation === 'subscription'
-    );
-  },
-  wsLink,
-  httpLink
-);
+const client = new ApolloClient({
+	link,
+	cache: new InMemoryCache()
+})
 
-// Create Apollo Client
-const gqlClient = new ApolloClient({
-  link: splitLink,
-  cache: new InMemoryCache(),
-});
-
-export default gqlClient;
-
+export default client
+ 
